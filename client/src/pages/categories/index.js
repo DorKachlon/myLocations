f4823d45083b11514d000e02f37a04a0953b7170
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-24 19:14:30 +0200

    categories page components with use effect to reset selected item

--- client/src/pages/categories/index.js
+++ client/src/pages/categories/index.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
 import CategoryCard from "../../components/categoryCard";
+import { useDispatch } from "react-redux";
+import { resetSelectedItem } from "../../actions/selectedItem";
 
 import "./style.css";
 
 export default function Categories() {
   const categories = useSelector((state) => state.categories);
   const selectedItem = useSelector((state) => state.selectedItem);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetSelectedItem());
+    };
+  }, []);
   return (
     <>
       {categories.length > 0 ? (
         <div className="page page-categories">
           <div className="categories-grid">
             {categories.map((category, i) => (
-              <CategoryCard key={i} category={category} selectedItem={selectedItem} />
+              <CategoryCard
+                key={i}
+                category={category}
+                selectedItemBool={selectedItem[0]?.id === category.id}
+              />
             ))}
           </div>
         </div>
