945c763f97a35084ce1c397253b2aef80c2a0fe9
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-24 12:09:47 +0200

    categories page grid layout

--- client/src/pages/categories/index.js
+++ client/src/pages/categories/index.js
@@ -3,16 +3,24 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
+import CategoryCard from "../../components/categoryCard";
+
 import "./style.css";
 export default function Categories() {
   const categories = useSelector((state) => state.categories);
   console.log(categories);
   return (
     <>
-      <div className="page page-categories">
-        {categories.length > 0 ? (
-          categories.map((category) => <div>{category.name}</div>)
-        ) : (
+      {categories.length > 0 ? (
+        <div className="page page-categories">
+          <div className="categories-grid">
+            {categories.map((category) => (
+              <CategoryCard category={category} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="page page-categories-empty">
           <div className="empty-section">
             <h2 className="title">Click here to add your first Category : </h2>
             <Link to="/add-category">
@@ -21,8 +29,8 @@ export default function Categories() {
               </Tooltip>
             </Link>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 }
