6d0636994efc931a0cab6284026c7416d7cdb02e
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-24 19:12:35 +0200

    basic location card

---
+++ client/src/components/locationCard.js/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setSelectedItem } from "../../actions/selectedItem";
+
+import "./style.css";
+export default function LocationCard({ location, selectedItemBool }) {
+  const dispatch = useDispatch();
+  const clickHandler = () => {
+    dispatch(setSelectedItem({ ...location, type: "location" }));
+  };
+  return (
+    <>
+      <div className={selectedItemBool ? "card-selected" : "card"} onClick={clickHandler}>
+        <div className="location-name">name: {location.name}</div>
+        <div className="location-name">address: {location.address}</div>
+        <div className="location-name">
+          coordinates: {location.coordinates.lng} | {location.coordinates.lat}
+        </div>
+        <div className="location-category-container">
+          {location.category.map((singleCategories) => (
+            <div className="location-category">{singleCategories}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
