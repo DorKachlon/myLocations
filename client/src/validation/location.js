c20512249afdbf35a21427a410b8ed5b11aa0c53
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-24 20:42:05 +0200

    min 1 value for category in location validation

--- client/src/validation/location.js
+++ client/src/validation/location.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export default async function locationValidation(data) {
   const schema = yup.object().shape({
-    category: yup.array().required(),
+    category: yup.array().min(1).required(),
     coordinatesLng: yup.number().required(),
     coordinatesLat: yup.number().required(),
     address: yup.string().required(),
