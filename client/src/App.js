d277264512462e263fed4658aa8b8391f1e51e90
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2021-02-25 13:12:56 +0200

    route of single category info

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Categories from "./pages/categories";
 import Locations from "./pages/locations";
 import AddCategory from "./pages/addCategory";
 import AddLocation from "./pages/addLocation";
+import SingleCategory from "./pages/singleCategory";
 
 import "./style/common.css";
 import "./style/color.css";
@@ -41,6 +42,7 @@ function App() {
           <Menu />
           <Switch>
             <Route exact path="/categories" component={Categories} />
+            <Route path="/categories/:id" component={SingleCategory} />
             <Route exact path="/locations" component={Locations} />
             <Route exact path="/add-category" component={AddCategory} />
             <Route exact path="/add-location" component={AddLocation} />
